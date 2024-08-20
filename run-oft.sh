0fc504b277d157f2332e8aea950487bdd416c723
Kai Hudalla <kai.hudalla@bosch.com>
2024-08-20 10:53:51 +0200

    Add CI workflow for linting artifacts
    Run ShellCheck on workspace files.

--- run-oft.sh
+++ run-oft.sh
@@ -9,6 +9,8 @@ files=$3
 echo "::notice::using OpenFastTrace JARs from: ${LIB_DIR}"
 
 # Run OpenFastTrace
+# shellcheck disable=SC2086
+# we need to provide the file patterns unquoted in order for the shell to expand any glob patterns like "*.md"
 if (java -cp "${LIB_DIR}/*" \
   org.itsallcode.openfasttrace.core.cli.CliStarter trace -o "${report_format}" \
   -f "${report_file_name}" \
