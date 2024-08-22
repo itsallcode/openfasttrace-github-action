996fd4a226bcdcd54a712d5236bc996126588fe5
Kai Hudalla <kai.hudalla@bosch.com>
2024-08-22 12:47:03 +0200

    Add input to fail Action on OFT error (#6)

--- run-oft.sh
+++ run-oft.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # [impl->req~run-oft-trace-command~1]
-
-report_file_name=$1
-report_format=$2
-files=$3
+fail_on_error=$1
+report_file_name=$2
+report_format=$3
+files=$4
 
 echo "::notice::using OpenFastTrace JARs from: ${LIB_DIR}"
 
@@ -19,6 +19,10 @@ then
   echo "oft-exit-code=0" >> "${GITHUB_OUTPUT}"
   echo "All specification items are covered." >> "${GITHUB_STEP_SUMMARY}"
 else
-  echo "oft-exit-code=$?" >> "${GITHUB_OUTPUT}"
+  oft_exit_code=$?
+  echo "oft-exit-code=${oft_exit_code}" >> "${GITHUB_OUTPUT}"
   echo "Some specification items are not covered. See created report (${report_file_name}) for details." >> "${GITHUB_STEP_SUMMARY}"
+  if [ "${fail_on_error}" = "true" ]; then
+    exit ${oft_exit_code}
+  fi
 fi
